ebe8d90eb5521e027f0b826f16fd08aa14b38426
matthewkuria <mathewkwachira@gmail.com>
2024-07-12 00:21:45 +0300

    Set the  Product Cards by using the dot map array method

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,9 +1,36 @@
 import Image from "next/image";
-
-export default function Home() {
+import data from "./lib/data.json"
+import { Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle, } from "./ui/components/ui/card";
+import { Button } from "./ui/components/ui/button";
+export default function Home() {  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl text-blue-600">Product list with cart </h1>
+    <main className="p-12 w-full">
+      <h1 className="font-bold my-5 text-3xl">Desserts</h1>
+      <div className="max-w-md flex flex-col md:flex md:flex-row md:flex-wrap gap-8">
+          {data.map((item) => {
+          return (
+            <Card className=" w-[320px] md:w-[120px] md:h-[220px] rounded-md basis-1 flex-auto">
+              <Image
+                src={item.image.desktop}
+                alt={item.name}
+                width={250}
+                height={150}
+                className="rounded-md flex-none hidden md:block"
+              />            
+              <CardFooter className="flex flex-col items-start text-left mt-2 text-xs ">
+                <p>{ item.category}</p>
+                <p>{ item.name}</p>
+                <p>${ item.price}</p>
+              </CardFooter>
+            </Card>
+          )
+        })}
+      </div>
     </main>
   );
 }
