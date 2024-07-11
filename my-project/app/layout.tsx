7009e461be8cb8193f7edf3a724117663676403c
matthewkuria <mathewkwachira@gmail.com>
2024-07-12 01:04:58 +0300

    Add padding to the i,mages

--- my-project/app/layout.tsx
+++ my-project/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { redHatText } from "@/app/ui/fonts";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={redHatText.className}>{children}</body>
     </html>
   );
 }
--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,41 +1,43 @@
 import Image from "next/image";
 import data from "./lib/data.json"
-import { Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle, } from "./ui/components/ui/card";
+import { Card,CardFooter,
+   } from "./ui/components/ui/card";
 import { Button } from "./ui/components/ui/button";
 export default function Home() {  
   return (
     <main className="p-12 w-full">
       <h1 className="font-bold my-5 text-3xl">Desserts</h1>
-      <div className="max-w-md flex flex-col md:flex md:flex-row md:flex-wrap gap-8">
+      <div className="max-w-md flex flex-col md:flex md:flex-row lg:flex md:flex-wrap gap-10 w-full">
           {data.map((item) => {
           return (
-            <Card className=" w-[320px] md:w-[120px] md:h-[220px] rounded-md basis-1 flex-auto">
+            <Card className=" w-[320px] md:w-[120px] lg:w-[120px] md:h-[220px] rounded-md basis-1 flex-auto ">
               {/* The image for the desktop design */}
               <Image
                 src={item.image.desktop}
                 alt={item.name}
-                width={250}
+                width={180}
                 height={150}
-                className="rounded-md flex-none hidden md:block"
+                className="rounded-md flex-none hidden md:hidden lg:block p-1"
               /> 
               {/* The image for the tablet design */}
-              
+              <Image
+                src={item.image.tablet}
+                alt={item.name}
+                width={350}
+                height={150}
+                className="rounded-md flex-none hidden md:block lg:hidden"
+              />
               {/* The image for the mobile design */}
               <Image
                 src={item.image.mobile}
                 alt={item.name}
                 width={350}
                 height={150}
-                className="rounded-md flex-none block md:hidden"
+                className="rounded-md flex-none block md:hidden lg:hidden"
               />
-              <CardFooter className="flex flex-col items-start text-left mt-2 text-xs ">
+              <CardFooter className="flex flex-col items-start text-left mt-2 text-[16px] px-1 ">
                 <p>{ item.category}</p>
-                <p>{ item.name}</p>
+                <p className="font-bold">{ item.name}</p>
                 <p className="font-bold">${ item.price}</p>
               </CardFooter>
             </Card>
