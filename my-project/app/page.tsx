c3739e5efca6a15d91bb2c7d2376f674ea3ab1fb
matthewkuria <mathewkwachira@gmail.com>
2024-07-28 16:52:40 +0300

    correct type for the item

--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -1,54 +1,27 @@
+"use client";
 import Image from "next/image";
 // Import the data in the data.json file
 import data from "./lib/data.json"
 import { Card,CardFooter,
-   } from "./ui/components/ui/card";
+} from "./ui/components/ui/card";
+import CardItem from "./ui/card-item";
 import { Button } from "./ui/components/ui/button";
-export default function Home() {  
+import { useState } from "react";
+export default function Home() { 
   return (
-    <main className="flex p-12 w-full bg-rose-100">
+    <main className="flex flex-col md:flex-row p-12 w-full bg-rose-100">
       <div className="flex flex-col">
         <h1 className="font-bold my-5 text-3xl">Desserts</h1>
         <div className="md:max-w-md lg:max-w-3xl flex flex-col md:flex md:flex-row lg:flex md:flex-wrap gap-10">
           {/* Map through the data from the JSON file and return a  card for each item */}
             {data.map((item) => {
-            return (
-              <Card className="drop-shadow-none border-none w-[320px] md:w-[120px] lg:w-[190px] md:h-[220px] lg:h-[290px] rounded-md basis-1 flex-auto ">
-                {/* The image for the desktop design */}
-                <Image
-                  src={item.image.desktop}
-                  alt={item.name}
-                  width={220}
-                  height={100}
-                  className="rounded-lg flex-none hidden md:hidden lg:block "
-                /> 
-                {/* The image for the tablet design */}
-                <Image
-                  src={item.image.tablet}
-                  alt={item.name}
-                  width={350}
-                  height={150}
-                  className="rounded-md flex-none hidden md:block lg:hidden"
-                />
-                {/* The image for the mobile design */}
-                <Image
-                  src={item.image.mobile}
-                  alt={item.name}
-                  width={350}
-                  height={150}
-                  className="rounded-md flex-none block md:hidden lg:hidden"
-                />
-                <CardFooter className="flex flex-col items-start text-left mt-2  px-1 font-bold ">
-                  <p className="font-normal text-rose-400">{ item.category}</p>
-                  <p className="text-[16px]">{ item.name}</p>
-                  <p className=" text-red">${ item.price}</p>
-                </CardFooter>
-              </Card>
+              return (
+                <CardItem key={item.name} item={item} />
             )
           })}
         </div>
       </div>
-      <div className="ml-64">
+      <div className="md:ml-56">
         <Card className="w-[280px] h-[300px] bg-white border-none">
           <p className="text-red text-[16px] font-bold p-4">Your Cart <span>(0)</span></p>
           <Image
---
+++ my-project/app/ui/card-item.tsx
@@ -0,0 +1,69 @@
+"use client"
+import Image from "next/image"
+import { Card, CardFooter } from "./components/ui/card"
+import { Button } from "./components/ui/button"
+import { useState } from "react"
+
+
+const CardItem = ({item}:any) => {
+  
+  const [showControls, setShowControls] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+  const handleAddToCartClick = () => {
+        setShowControls(true);
+    }; 
+    return (
+        <Card key={item.name} className="drop-shadow-none border-none w-[320px] md:w-[120px] lg:w-[190px] md:h-[220px] lg:h-[290px] rounded-md basis-1 flex-auto ">
+                {/* The image for the desktop design */}
+                <Image
+                  src={item.image.desktop}
+                  alt={item.name}
+                  width={220}
+                  height={100}
+                  className="rounded-lg flex-none hidden md:hidden lg:block mx-auto"
+                /> 
+                {/* The image for the tablet design */}
+                <Image
+                  src={item.image.tablet}
+                  alt={item.name}
+                  width={350}
+                  height={150}
+                  className="rounded-md flex-none hidden md:block lg:hidden"
+                />
+                {/* The image for the mobile design */}
+                <Image
+                  src={item.image.mobile}
+                  alt={item.name}
+                  width={350}
+                  height={150}
+                  className="rounded-md flex-none block md:hidden lg:hidden"
+                />              
+                <Button
+                  onClick={handleAddToCartClick}
+                  className="flex mx-auto border border-red px-6 py-1 hover:bg-red hover:text-white font-bold ">
+                  <Image
+                    src="/assets/images/icon-add-to-cart.svg"
+                    alt="The add to cart icon for the button"
+                    width={20}
+                    height={10}
+                    className=""
+                  />
+                    {showControls ? (
+                    <div className="controls">
+                        <button  className="inner-btn">-</button>
+                        <span className="quantity">{quantity}</span>
+                        <button  className="inner-btn">+</button>
+                    </div>
+                ) : (
+                    'Add to Cart'
+                )}
+                </Button>
+                <CardFooter className="flex flex-col items-start text-left mt-2  px-1 font-bold ">
+                  <p className="font-normal text-rose-400">{ item.category}</p>
+                  <p className="text-[16px]">{ item.name}</p>
+                  <p className=" text-red">${ item.price}</p>
+                </CardFooter>
+              </Card>
+    )
+}
+export default CardItem
