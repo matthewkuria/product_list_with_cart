2457656c84f2266af7e348686a493379a053c1f1
matthewkuria <mathewkwachira@gmail.com>
2024-08-16 23:40:10 +0300

    Add red rose color to the prrice

--- my-project/app/ui/cart.tsx
+++ my-project/app/ui/cart.tsx
@@ -22,13 +22,13 @@ const Cart = () => {
                 ) : (
                     <ul>
                         {cartItems.map((item: { id: React.Key | null | undefined; name: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; price: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; quantity: string | number | readonly string[] | undefined; }) => (
-                            <div  key={item.id} className='flex flex-row text-sm justify-between '>
+                            <div  key={item.id} className='flex flex-row text-sm justify-between p-5 '>
                                 <div className="flex flex-col">
                                     <h4>{item.name}</h4>
-                                    <div className="flex justify-around">
-                                        <p className='text-red'>x{ item.quantity}</p>
+                                    <div className="flex justify-around text-rose-300 ">
+                                        <p className='text-red font-bold'>x{ item.quantity}</p>
                                         <p>@${item.price}</p>
-                                        <p className="">${item.price }</p>
+                                        <p className="font-bold">${item.price }</p>
                                     </div>
                                 </div>
                                 <button className='font-bold' onClick={() => removeFromCart(item.id)}>X</button>
